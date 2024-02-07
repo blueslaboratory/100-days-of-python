5437e74d50d545f8861f3a67898bac16b0f2a73c
blueslaboratory <myjunkandspam22@gmail.com>
2024-02-07 19:16:13 +0100

    day 6

---
+++ 006/functions.py
@@ -0,0 +1,23 @@
+# 07/02/2024
+# Day - 006
+
+
+
+##################################################
+# https://docs.python.org/3/library/functions.html
+# https://reeborg.ca/reeborg.html
+print("\n\n*** Functions ***")
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+
+
+# https://peps.python.org/pep-0008/#tabs-or-spaces
+print("\n*** Indentation ***")
+print("IMPORTANT: Python 3 disallows mixing the use of tabs and spaces for indentation")
+print("IMPORTANT: indentation should be done with 4 spaces")
+print("VSC: tab key == 4 spaces")
