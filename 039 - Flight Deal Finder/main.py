c63b37eb3d7bc93ec34496818fb47a87c7a1177c
blueslaboratory <myjunkandspam22@gmail.com>
2024-06-27 16:27:16 +0200

    day 39

--- 039 - Flight Deal Finder/main.py
+++ 039 - Flight Deal Finder/main.py
@@ -1,8 +1,8 @@
 # 26/06/2024
 # Day - 039
 
-# This file will need to use the DataManager, FlightSearch, 
-# FlightData, NotificationManager classes to achieve the program 
+# This file will need to use the DataManager, FlightSearch,
+# FlightData, NotificationManager classes to achieve the program
 # requirements.
 
 ##################################################
@@ -10,20 +10,30 @@
 
 print("\n*** Welcome to the Flight Deal Finder! ***")
 
+import time
+from datetime import datetime, timedelta
 from pprint import pprint
 from data_manager import DataManager
 from flight_search import FlightSearch
-import time
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
+
 
 
 # ==================== Set up the Flight Search ====================
+
 # Create an instance of DataManager
 data_manager = DataManager()
 # Fetch the sheet data
 sheet_data = data_manager.get_destination_data()
 # Print the sheet data
 pprint(sheet_data)
+# Flight Search
 flight_search = FlightSearch()
+# Set your origin airport
+ORIGIN_CITY_IATA = "MAD"
+# Notification Manager
+notification_manager = NotificationManager()
 
 
 
@@ -41,7 +51,54 @@ for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
         # slowing down requests to avoid rate limit
         time.sleep(2)
+
 print(f"sheet_data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+
+
+# ==================== Search for Flights ====================
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+
+    print(f"Getting flights for {destination['city']}...")
+
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: {cheapest_flight.price}€")
+
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
+
+
+    # ==================== Send SMS/Whatsapp/Email ====================
+
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+
+        message= (
+            f"Low price alert! Only {cheapest_flight.price}€ to fly "
+            f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+            f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
+
+        # Send SMS
+        # notification_manager.send_sms(message_body=message)
+
+        # SMS not working? Try whatsapp instead.
+        # notification_manager.send_whatsapp(message_body=message)
+
+        # Whatsapp not working? Try email, email never fails.
+        notification_manager.send_email(message_body=message)
--- 039 - Flight Deal Finder/notification_manager.py
+++ 039 - Flight Deal Finder/notification_manager.py
@@ -5,7 +5,88 @@
 # with the deal flight details.
 
 ##################################################
-# DAY 39 PROJECT: 
+# DAY 39 PROJECT: SOLUTION
+
+
+import os
+from twilio.rest import Client
+# pip install twilio
+
+# Email
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+sender = "titiritest@gmail.com"
+receiver = sender
+pw = "pknf dczx zfpd qleg"
+
+# Using a .env file to retrieve the phone numbers and tokens.
 
 class NotificationManager:
-   pass
+
+    def __init__(self):
+        # self.client = Client(os.environ['TWILIO_SID'], os.environ["TWILIO_AUTH_TOKEN"])
+        pass
+
+    def send_sms(self, message_body):
+        """
+        Sends an SMS message through the Twilio API.
+        This function takes a message body as input and uses the Twilio API to send an SMS from
+        a predefined virtual number (provided by Twilio) to your own "verified" number.
+        It logs the unique SID (Session ID) of the message, which can be used to
+        verify that the message was sent successfully.
+        
+        Parameters:
+        message_body (str): The text content of the SMS message to be sent.
+        
+        Returns:
+        None
+        
+        Notes:
+        - Ensure that `TWILIO_VIRTUAL_NUMBER` and `TWILIO_VERIFIED_NUMBER` are correctly set up in
+        your environment (.env file) and correspond with numbers registered and verified in your
+        Twilio account.
+        - The Twilio client (`self.client`) should be initialized and authenticated with your
+        Twilio account credentials prior to using this function when the Notification Manager gets
+        initialized.
+        """
+        message = self.client.messages.create(
+            from_=os.environ["TWILIO_VIRTUAL_NUMBER"],
+            body=message_body,
+            to=os.environ["TWILIO_VIRTUAL_NUMBER"]
+        )
+        # Prints if successfully sent.
+        print(message.sid)
+
+    # Is SMS not working for you or prefer whatsapp? Connect to the WhatsApp Sandbox!
+    # https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn
+    def send_whatsapp(self, message_body):
+        message = self.client.messages.create(
+            from_=f'whatsapp:{os.environ["TWILIO_WHATSAPP_NUMBER"]}',
+            body=message_body,
+            to=f'whatsapp:{os.environ["TWILIO_VERIFIED_NUMBER"]}'
+        )
+        print(message.sid)
+
+
+    def send_email(self, message_body):
+        # Encode the message in UTF-8
+        message = message_body.encode('utf-8')
+
+        # Create a MIMEText object to represent the email
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = sender
+        msg['Subject'] = "Low price alert!"
+
+        msg.attach(MIMEText(message, 'plain', 'utf-8'))
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=sender, password=pw)
+            connection.sendmail(
+                from_addr=msg['From'],
+                to_addrs=msg['To'],
+                msg=msg.as_string()
+            )
