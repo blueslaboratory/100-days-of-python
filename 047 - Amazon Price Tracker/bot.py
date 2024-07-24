0d069f757a651da87baf53e0c16ff4c7948c8a98
blueslaboratory <myjunkandspam22@gmail.com>
2024-07-24 08:46:22 +0200

    day 47

--- 047 - Amazon Price Tracker/bot.py
+++ 047 - Amazon Price Tracker/bot.py
@@ -26,24 +26,24 @@ practice_URL = "https://appbrewery.github.io/instant_pot/"
 live_URL = "https://www.amazon.com/Funko-Articulated-Rick-Morty-Mr-Poopy/dp/B01MY7YTNR"
 
 # https://httpbin.org/headers
-all_headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "es-ES,es",
-    "Host": "httpbin.org",
-    "Priority": "u=0, i",
-    "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Brave\";v=\"126\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "\"Windows\"",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1",
-    "Sec-Gpc": "1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    "X-Amzn-Trace-Id": "Root=1-669f9710-36f194065132d2735aa56ee5"
-  }
+# all_headers = {
+#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+#     "Accept-Encoding": "gzip, deflate, br, zstd",
+#     "Accept-Language": "es-ES,es",
+#     "Host": "httpbin.org",
+#     "Priority": "u=0, i",
+#     "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Brave\";v=\"126\"",
+#     "Sec-Ch-Ua-Mobile": "?0",
+#     "Sec-Ch-Ua-Platform": "\"Windows\"",
+#     "Sec-Fetch-Dest": "document",
+#     "Sec-Fetch-Mode": "navigate",
+#     "Sec-Fetch-Site": "cross-site",
+#     "Sec-Fetch-User": "?1",
+#     "Sec-Gpc": "1",
+#     "Upgrade-Insecure-Requests": "1",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+#     "X-Amzn-Trace-Id": "Root=1-669f9710-36f194065132d2735aa56ee5"
+#   }
 
 # response = requests.get(practice_URL, verify=False, headers=all_headers)
 response = requests.get(practice_URL, verify=False, headers={"Accept-Language":"en-US"})
